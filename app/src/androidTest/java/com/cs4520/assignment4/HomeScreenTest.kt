bc69694857e0c775bf70e037cbb99c1dc632c9db
pthaipat <thaipat.p@northeastern.edu>
2024-04-19 23:53:39 -0400

    Additional

--- app/src/androidTest/java/com/cs4520/assignment4/HomeScreenTest.kt
+++ app/src/androidTest/java/com/cs4520/assignment4/HomeScreenTest.kt
@@ -8,6 +8,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.cs4520.assignment4.screens.HomeScreen
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
